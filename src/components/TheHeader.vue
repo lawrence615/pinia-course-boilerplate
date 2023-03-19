b177967c0fbd03a6bca8c75fd4622a2671539776
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-03-19 09:59:34 +0300

    mapping the state to a different name

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -8,7 +8,9 @@ export default {
   name: "TheHeader",
   components: { CartWidget },
   computed: {
-    ...mapState(useAuth, ["username"]),
+    ...mapState(useAuth, {
+      user: "username",
+    }),
   },
 };
 </script>
@@ -20,8 +22,8 @@ export default {
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
     <div>
-      <span class="mr-5">{{ username }}</span>
-      <CartWidget class="inline-block"/>
+      <span class="mr-5">{{ user }}</span>
+      <CartWidget class="inline-block" />
     </div>
   </header>
 </template>
