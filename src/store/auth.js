ce716530abc0ac2ee9739b3860e7179ccb2f2664
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-03-19 10:23:24 +0300

    accessing getters from a Pinia store using the options API

--- src/store/auth.js
+++ src/store/auth.js
@@ -3,7 +3,10 @@ import { defineStore } from "pinia";
 export const useAuth = defineStore("auth", {
     state:()=>{
         return {
-            username:'wyze'
+            // username:'wyze'
         }
+    },
+    getters:{
+        username:() => 'wyze2'
     }
 });
