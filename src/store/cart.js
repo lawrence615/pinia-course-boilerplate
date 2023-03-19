16010cc7c61efead2f4c14e2c7ecbe05da408f7c
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-03-19 05:24:40 +0300

    grouped items in items array and returns them as grouped

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { groupBy } from "lodash";
 
 export const useCart = defineStore("cart", {
   state: () => {
@@ -9,6 +10,7 @@ export const useCart = defineStore("cart", {
   getters: {
     count: (state) => state.items.length,
     isEmpty: (state) => state.count === 0,
+    grouped: state => groupBy(state.items, item => item.name)
   },
   actions: {
     addItems(count, item) {
