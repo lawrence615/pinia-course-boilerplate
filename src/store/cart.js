65dbd48097317083b50244fc82bdcfd87705d669
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-03-19 04:21:44 +0300

    Update
    Objects are passed by reference, and there when an update is made to one, all instance of the same also changes. This is a major bug, To fix this we need to clone item before adding to cart items. Spreading item into tts own object

--- src/store/cart.js
+++ src/store/cart.js
@@ -11,7 +11,7 @@ export const useCart = defineStore("cart", {
       count = parseInt(count);
 
       for (let index = 0; index < count; index++) {
-        this.items.push(item);
+        this.items.push({...item});
       }
     },
   },
