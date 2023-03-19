75ceb8137a0c80ee4c4a3ae9403b8b6c4c2b1fb7
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-03-19 19:51:54 +0300

    added use of local storage to store items in a cart

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,4 +1,7 @@
 import { defineStore, acceptHMRUpdate } from "pinia";
+
+import { useLocalStorage } from "@vueuse/core";
+
 import { groupBy } from "lodash";
 
 import { useAuth } from "@/store/auth";
@@ -7,7 +10,7 @@ export const useCart = defineStore("cart", {
   historyEnabled: true,
   state: () => {
     return {
-      items: [],
+      items: useLocalStorage("cart:items", []),
     };
   },
   getters: {
