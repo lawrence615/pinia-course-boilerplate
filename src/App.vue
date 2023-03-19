90459bf49b3040c72549d3f6a3396fb839892fb1
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-03-19 18:11:30 +0300

    Update
    - deleted commented code
    - calling undo & redo functions from cartStore

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref, reactive } from "vue";
 
 import TheHeader from "@/components/TheHeader.vue";
 import AppButton from "@/components/AppButton.vue";
@@ -9,56 +8,6 @@ import { useCart } from "./store/cart";
 
 const productStore = useProduct();
 const cartStore = useCart();
-
-productStore.fill();
-
-const history = reactive([]);
-
-const future = reactive([]);
-
-const doingHistory = ref(false);
-
-history.push(JSON.stringify(cartStore.$state));
-
-const undo = () => {
-  if (history.length === 1) return;
-
-  doingHistory.value = true;
-
-  future.push(history.pop());
-
-  cartStore.$state = JSON.parse(history.at(-1));
-
-  doingHistory.value = false;
-};
-
-const redo = () => {
-  // pop the last element from the future array as it's the last element to be undone
-  const latestState = future.pop();
-
-  // if the no latest state. we return as there is nothing to redo
-  if (!latestState) return;
-
-  // set doingHistory to true
-  doingHistory.value = true;
-
-  // push the latest state back on to the history for it to available for undo again
-  history.push(latestState);
-
-  // replace cartStore.$state with the latestState
-  cartStore.$state = JSON.parse(latestState);
-
-  // set doingHistory to false
-  doingHistory.value = false;
-};
-
-cartStore.$subscribe((mutations, state) => {
-  if (!doingHistory.value) {
-    history.push(JSON.stringify(state));
-
-    future.splice(0,future.length)
-  }
-});
 
 cartStore.$onAction(({ name, store, args, after, onError }) => {
   if (name === "addItems") {
@@ -71,6 +20,9 @@ cartStore.$onAction(({ name, store, args, after, onError }) => {
     });
   }
 });
+
+productStore.fill();
+
 </script>
 
 <template>
@@ -78,8 +30,8 @@ cartStore.$onAction(({ name, store, args, after, onError }) => {
     <TheHeader />
 
     <div class="mb-5 flex justify-end">
-      <AppButton @click="undo">Undo</AppButton>
-      <AppButton class="ml-2" @click="redo">Redo</AppButton>
+      <AppButton @click="cartStore.undo">Undo</AppButton>
+      <AppButton class="ml-2" @click="cartStore.redo">Redo</AppButton>
     </div>
 
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
