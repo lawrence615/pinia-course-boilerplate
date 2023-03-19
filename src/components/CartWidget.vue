5d7c627613cbfe896dd2292ae47cf8a844b6f2b1
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-03-19 09:45:46 +0300

    When Checkout function is clicked, it calls checkout function

--- src/components/CartWidget.vue
+++ src/components/CartWidget.vue
@@ -37,7 +37,7 @@ const cartStore = useCart();
           <AppButton class="secondary mr-2" @click="cartStore.$reset()"
             >Clear Cart</AppButton
           >
-          <AppButton class="primary">Checkout</AppButton>
+          <AppButton class="primary" @click="cartStore.checkout">Checkout</AppButton>
         </div>
       </div>
       <!-- Uncomment and use condition to show when cart is empty -->
